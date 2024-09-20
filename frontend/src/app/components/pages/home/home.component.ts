45252a7331d39878c66069f891c2a4198d9de5fc
billythephes <21521291@gm.uit.edu.vn>
2024-09-20 23:27:20 +0700

    handle search function

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
 import { Food } from '../../../shared/models/Food';
 import { FoodService } from '../../../services/food.service';
-import { RouterModule } from '@angular/router';
+import { RouterModule, ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { StarRatingComponent } from "../../partials/star-rating/star-rating.component";
+import { SearchComponent } from "../../partials/search/search.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, CommonModule, StarRatingComponent],
+  imports: [RouterModule, CommonModule, StarRatingComponent, SearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
   foods:Food[] = [];
-  constructor(private foodService:FoodService) {
-    this.foods = foodService.getAll();
+  constructor(private foodService:FoodService, activatedRoute:ActivatedRoute) {
+    activatedRoute.params.subscribe((params) => {
+      if(params.searchTerm)
+        this.foods = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      else
+        this.foods = foodService.getAll();
+    })  
   }
 
 }
