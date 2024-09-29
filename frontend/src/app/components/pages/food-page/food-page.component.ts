250238dfe96663fa6ce1732ac6303d58de58e6c1
billythephes <21521291@gm.uit.edu.vn>
2024-09-30 00:54:05 +0700

    Add food page route, service & component

---
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Food } from '../../../shared/models/Food';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { FoodService } from '../../../services/food.service';
+import { CommonModule } from '@angular/common';
+import { StarRatingComponent } from "../../partials/star-rating/star-rating.component";
+
+@Component({
+  selector: 'app-food-page',
+  standalone: true,
+  imports: [CommonModule, RouterModule, StarRatingComponent],
+  templateUrl: './food-page.component.html',
+  styleUrl: './food-page.component.css'
+})
+export class FoodPageComponent {
+  food!: Food;
+  constructor(activatedRoute:ActivatedRoute, foodService:FoodService) {
+    activatedRoute.params.subscribe((params) => {
+      if(params.id)
+        this.food = foodService.getFoodById(params.id);
+    })
+  }
+}
