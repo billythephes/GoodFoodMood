b69a5180a4a4194a17108143a88d7d274a4756a2
billythephes <21521291@gm.uit.edu.vn>
2024-11-18 17:56:18 +0700

    Update register link & function

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -3,6 +3,8 @@ import { sample_users } from "../data";
 import jwt from "jsonwebtoken";
 import asyncHandle from 'express-async-handler'
 import { User, UserModel } from "../models/user.model";
+import { HTTP_BAD_REQUEST } from "../constants/http_status";
+import bcrypt from 'bcryptjs';
 
 const router = Router();
 
@@ -27,8 +29,33 @@ router.post("/login", asyncHandle(
             res.send(generateTokenResponse(user));
         } 
         else {
-            res.status(400).send("Username or password is not valid!");
+            res.status(HTTP_BAD_REQUEST).send("Username or password is not valid!");
         }
+    }
+))
+
+router.post('/register', asyncHandle(
+    async (req, res) => {
+        const {name, email, password, address} = req.body;
+        const user = await UserModel.findOne({email});
+        if (user) {
+            res.status(HTTP_BAD_REQUEST).send('User is already exist, please login!');
+            return;
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        const newUser:User = {
+            id:'',
+            name,
+            email: email.toLowerCase(),
+            password: encryptedPassword,
+            address,
+            isAdmin: false
+        }
+
+        const dbUser = await UserModel.create(newUser);
+        res.send(generateTokenResponse(dbUser));
     }
 ))
 
