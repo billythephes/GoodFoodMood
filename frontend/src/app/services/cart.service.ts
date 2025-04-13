bf6ed07e40cf1472c7830a6dba9c239365408a56
Phes <nguyenhongphuc41003@gmail.com>
2025-04-13 22:00:23 +0700

    Update cart service

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -3,28 +3,53 @@ import { Cart } from '../shared/models/Cart';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Food } from '../shared/models/Food';
 import { CartItem } from '../shared/models/CartItem';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  private cart:Cart = this.getCartFromLocalStorage();
+  private cart: Cart = new Cart();
   private cartSubject: BehaviorSubject<Cart> = new BehaviorSubject(this.cart);
 
-  constructor() { }
+  constructor(private userService: UserService) {
+    this.initializeCart();
+    
+    this.userService.userObservable.subscribe(user => {
+      if (user.token) {
+        const cartJson = localStorage.getItem(`Cart_${user.id}`);
+        if (cartJson) {
+          this.cart = JSON.parse(cartJson);
+          this.cartSubject.next(this.cart);
+        }
+      }
+    });
+  }
+
+  initializeCart() {
+    const user = this.userService.currentUser;
+    if (user.id) {
+      const cartJson = localStorage.getItem(`Cart_${user.id}`);
+      if (cartJson) {
+        this.cart = JSON.parse(cartJson);
+        this.cartSubject.next(this.cart);
+      }
+    }
+  }
 
   addToCart(food: Food): void {
-    let cartItem = this.cart.items.find(item => item.food.id === food.id)
-    if (cartItem)
-      return;
-
-    this.cart.items.push(new CartItem(food));
-    this.setCartToLocalStorage();
+    let cartItem = this.cart.items.find(item => item.food.id === food.id);
+    if (cartItem) {
+      cartItem.quantity++;
+    } else {
+      this.cart.items.push(new CartItem(food));
+    }
+    this.saveCartToLocalStorage();
   }
 
   removeFromCart(foodId: string): void {
     this.cart.items = this.cart.items.filter(item => item.food.id != foodId);
-    this.setCartToLocalStorage();
+    this.saveCartToLocalStorage();
   }
   
   changeQuantity(foodId: string, quantity: number) {
@@ -34,12 +59,12 @@ export class CartService {
 
     cartItem.quantity = quantity;
     cartItem.price = quantity * cartItem.food.price;
-    this.setCartToLocalStorage();
+    this.saveCartToLocalStorage();
   }
 
   clearCart() {
     this.cart = new Cart();
-    this.setCartToLocalStorage();
+    this.saveCartToLocalStorage();
   }
 
   getCartObservable(): Observable<Cart> {
@@ -50,16 +75,13 @@ export class CartService {
     return this.cartSubject.value;
   }
 
-  private setCartToLocalStorage(): void {
-    this.cart.totalPrice = this.cart.items.reduce((prevSum, currentItem) => prevSum + currentItem.price, 0);
-    this.cart.totalCount = this.cart.items.reduce((prevSum, currentItem) => prevSum + currentItem.quantity, 0);
-    const cartJson = JSON.stringify(this.cart);
-    localStorage.setItem('Cart', cartJson);
-    this.cartSubject.next(this.cart);
-  }
-
-  private getCartFromLocalStorage(): Cart {
-    const cartJson = localStorage.getItem('Cart');
-    return cartJson? JSON.parse(cartJson): new Cart()
+  private saveCartToLocalStorage() {
+    const user = this.userService.currentUser;
+    if (user.id) {
+      this.cart.totalPrice = this.cart.items.reduce((sum, item) => sum + item.price, 0);
+      this.cart.totalCount = this.cart.items.reduce((sum, item) => sum + item.quantity, 0);
+      localStorage.setItem(`Cart_${user.id}`, JSON.stringify(this.cart));
+      this.cartSubject.next(this.cart);
+    }
   }
 }
